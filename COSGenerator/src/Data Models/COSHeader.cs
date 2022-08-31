0b29ab6549f7fd71c415e0dacc339a95b1a56ca6
joshman1019 <joshman1019@gmail.com>
2022-08-31 16:55:34 -0400

    Added nullable string to ServiceDocumentDescription property.

--- COSGenerator/src/Data Models/COSHeader.cs
+++ COSGenerator/src/Data Models/COSHeader.cs
@@ -5,7 +5,7 @@ public class COSHeader
     public string? CaseNumber { get; set; }
     public List<string> Plaintiffs { get; set; } = new List<string>();
     public List<string> Defendants { get; set; } = new List<string>();
-    public string? InTheMatterOf { get; set; } = string.Empty; 
-    public string ServedDocumentDescription { get; set; }
+    public string? InTheMatterOf { get; set; } = string.Empty;
+    public string? ServedDocumentDescription { get; set; } = string.Empty;
 
 }
